d2bb27acf971047f5ec38f952c68820cf8caaa27
jake31may <107139790+jake31may@users.noreply.github.com>
2022-09-23 17:07:32 +0100

    Update Converter2.m

--- PAWEC/Converter2.m
+++ PAWEC/Converter2.m
@@ -10,12 +10,3 @@ FSkCw = Mat(:,4);
 Mat2 = [FSRAO,FSAveragePower,FSkCw];
 
 end
-
-%%
-% [fSCy2, SAvPowerCy2,SRAOCy2,SkCwCy2] = Converter2(fSCy,SGCy,49.39)
-% [fSCo2, SAvPowerCo2,SRAOCo2,SkCwCo2] = Converter2(fSCo,SGCo,49.39)
-% [fSDi2, SAvPowerDi2,SRAODi2,SkCwDi2] = Converter2(fSDi,SGDi,49.39)
-
-% [fBCy2, BAvPowerCy2,BRAOCy2,BkCwCy2] = Converter2(fBCy,BGCy,49.39)
-% [fBCo2, BAvPowerCo2,BRAOCo2,BkCwCo2] = Converter2(fBCo,BGCo,49.39)
-% [fBDi2, BAvPowerDi2,BRAODi2,BkCwDi2] = Converter2(fBDi,BGDi,49.39)
